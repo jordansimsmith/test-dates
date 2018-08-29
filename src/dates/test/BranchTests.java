632ef80704c5b850f7986e6d4ab7ffa5cb4d9df3
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-08-29 13:28:01 +1200

    Implemented 3 further test cases to satisfy branch coverage for the validity branches and the throw exception branch

--- src/dates/test/BranchTests.java
+++ src/dates/test/BranchTests.java
@@ -3,6 +3,7 @@ package dates.test;
 import dates.Dates;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 /**
@@ -61,7 +62,7 @@ public class BranchTests {
     }
 
     /**
-     * THis test method satisfies the fourth leap year branch, that the year is not divisible by 40, 100 or 400.
+     * This test method satisfies the fourth leap year branch, that the year is not divisible by 40, 100 or 400.
      * The method also tests that a date with the day exceeding the month's length is deemed invalid.
      * This test case checks that an IllegalArgumentException is thrown when a day exceeding the month length is parsed.
      * @throws Exception
@@ -75,4 +76,47 @@ public class BranchTests {
             // do nothing
         }
     }
+
+    /**
+     * This test method satisfies the branch logic where there the 30th day of february in a leap year is attempted.
+     * This is an invalid date as february only has 29 days in a leap year.
+     * The test case checks that an IllegalArgumentException is thrown when the 30th of february is parsed in a leap year.
+     * @throws Exception
+     */
+    @Test
+    public void testFebruaryLeapYear30Days() throws Exception {
+        try {
+            Dates.dayOfWeek(2000, 2,30);
+            fail();
+        } catch (IllegalArgumentException e) {
+            // do nothing
+        }
+    }
+
+    /**
+     * This test method executes the branch where a valid day on a february leap year is inputted.
+     * The dayOfWeek() method should correctly identify that this date is a Tuesday.
+     * This test case checks that this date is found to be valid (no exceptions thrown) and that the correct date is returned.
+     * @throws Exception
+     */
+    @Test
+    public void testValidFebruaryDateLeapYear() throws Exception {
+        Dates.Day day = Dates.dayOfWeek(2000, 2, 1);
+
+        assertEquals(Dates.Day.Tuesday, day);
+    }
+
+    /**
+     * This test method executes the branch where valid day on a non leap year is inputted.
+     * The dayOfWeek() method should correctly identify that this date is a Saturday.
+     * This test case checks that the date is found to be valid (no exceptions thrown) and that the correct date is returned.
+     * @throws Exception
+     */
+    @Test
+    public void testValidNonLeapYearDay() throws Exception {
+        Dates.Day day = Dates.dayOfWeek(1999, 6, 19);
+
+        assertEquals(Dates.Day.Saturday, day);
+    }
+
 }
