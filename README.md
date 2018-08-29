7b9797b7f3f1699a614eca5f096def5b348c82e0
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-08-29 12:18:33 +1200

    Set up working directory

---
+++ README.md
@@ -0,0 +1,2 @@
+# softeng254-a02
+Quality Assurance: Unit testing with three test coverages
---
+++ src/dates/test/BranchTests.java
@@ -0,0 +1,11 @@
+package dates.test;
+
+import org.junit.Test;
+
+public class BranchTests {
+
+    @Test
+    public void test() {
+        System.out.println("Test");
+    }
+}
